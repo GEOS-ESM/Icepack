0d32fdfae1272231fa68238483dbd921c0b964ab
bzhao <Bin.Zhao@nasa.gov>
2023-11-02 13:34:10 -0400

    update grid cell info

--- columnphysics/icepack_therm_shared.F90
+++ columnphysics/icepack_therm_shared.F90
@@ -563,8 +563,8 @@
 
          logical(kind=log_kind) :: ret
 
-         ret = (local_tsk == 89 .and. local_i == 18 .and. &
-                local_j == 81 .and. local_blk == 1)
+         ret = (local_tsk == 544 .and. local_i == 40 .and. &
+                local_j == 34 .and. local_blk == 1)
 
       end function ismyturn
 #endif
